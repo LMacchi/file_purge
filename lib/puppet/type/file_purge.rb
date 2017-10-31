bc9e446b9ab549e55b067aa3180d0a86a0b7a8ed
Laura Macchi <lau.macchi@gmail.com>
2017-10-31 16:06:35 -0500

    Add data validation

--- lib/puppet/type/file_purge.rb
+++ lib/puppet/type/file_purge.rb
@@ -1,6 +1,20 @@
 # file_purge/lib/puppet/type/file_purge.rb
 Puppet::Type.newtype(:file_purge) do
+  desc "Puppet type that tells Puppet to start monitoring a directory for unwanted files"
+
   ensurable()
-  newparam(:target, :namevar => true)
-  newparam(:whitelist)
+
+  newparam(:target, :namevar => true) do
+    desc "Directory to monitor - Must be a fully qualified path"
+    validate do |value|
+      p = Pathname.new(value)
+      if ! p.absolute?
+        raise ArgumentError, "Target must be an absolute path"
+      elsif ! p.directory?
+        raise ArgumentError, "Target %s does not exist or is not a directory" % value
+      end
+    end
+  end
+
+  newparam(:whitelist, :array_matching => :all)
 end
