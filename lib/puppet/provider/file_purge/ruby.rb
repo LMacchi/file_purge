223599a45d62c4007b2ea8ee11c4227a63ce5a9f
Laura Macchi <lm@puppet.com>
2017-11-01 19:33:59 +0000

    Restructure of provider methods for legibility

--- lib/puppet/provider/file_purge/ruby.rb
+++ lib/puppet/provider/file_purge/ruby.rb
@@ -2,13 +2,17 @@ Puppet::Type.type(:file_purge).provide(:ruby) do
 
   # Check if there are files to purge
   def exists?
-    get_files_to_purge(select_all_matching(@resource[:whitelist])).length == 0
+    all     = list_all_files_in_dir(@resource[:target])
+    to_keep = select_all_matching(all, @resource[:whitelist])
+    get_files_to_purge(all, to_keep).length == 0
   end
 
   # Get a list of files to delete and purge them
   def create
-    fp = get_files_to_purge(select_all_matching(@resource[:whitelist]))
-    purge_files(fp)
+    all      = list_all_files_in_dir(@resource[:target])
+    to_keep  = select_all_matching(all, @resource[:whitelist])
+    to_purge = get_files_to_purge(all, to_keep)
+    purge_files(to_purge)
   end
 
   # Do nothing
@@ -16,23 +20,22 @@ Puppet::Type.type(:file_purge).provide(:ruby) do
   end
 
   # Get a list of all files inside the target
-  def list_all_files_in_dir()
-    Dir.glob("#{@resource[:target]}/*")
+  def list_all_files_in_dir(target)
+    Dir.glob("#{target}/*")
   end
 
   # Given a list of patterns, return all matching files
-  def select_all_matching(patterns)
+  def select_all_matching(files, patterns)
     to_keep = []
     patterns.each do |pattern|
-      to_keep.concat(select_by_pattern(/#{pattern}/))
+      to_keep.concat(select_by_pattern(files, /#{pattern}/))
     end
     to_keep.uniq
   end
   
   # Given one pattern, return all matching files
-  def select_by_pattern(pattern)
+  def select_by_pattern(files, pattern)
     to_keep = []
-    files   = list_all_files_in_dir()
     files.each do |f|
       if f =~ pattern
         to_keep.push(f)
@@ -42,14 +45,14 @@ Puppet::Type.type(:file_purge).provide(:ruby) do
   end
 
   # Given all files and matching ones, return non-matching files
-  def get_files_to_purge(to_keep)
-    list_all_files_in_dir() - to_keep
+  def get_files_to_purge(files, to_keep)
+    to_purge = files - to_keep
+    to_purge
   end
 
   # Given a list of files, attempt to delete them
-  def purge_files(to_keep)
+  def purge_files(to_purge)
     Puppet.debug("Files to purge: #{to_purge}")
-    to.purge = get_files_to_purge(to_keep)
     to_purge.each do |p|
       begin
         File.delete(p)
